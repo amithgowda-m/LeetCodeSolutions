d3ed2e86d492bd966059252ed8666714993e0aa5
Amith Gowda <amithgowdasmg@gmail.com>
2026-07-23 22:02:58 +0530

    [3708] [Time Beats: 100.00%] [Memory Beats: 47.53%] - LeetPush

---
+++ 3708-Longest-Fibonacci-Subarray.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int longestSubarray(vector<int>& nums) {
+        int n = nums.size();
+        if(n <3) return n;
+        int curLen =2;
+        int maxLen =2;
+        for(int i=2;i<n;i++){
+            if(nums[i]==nums[i-1]+nums[i-2]){
+                curLen++;
+                maxLen = max(curLen,maxLen);
+            }
+            else curLen=2;
+        }
+        return maxLen;
+    }
+};
