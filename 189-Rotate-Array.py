a89d926694994147a85cad2b121587a4f40e429e
Amith Gowda <amithgowdasmg@gmail.com>
2026-08-05 21:21:28 +0530

    [189] [Time Beats: 100.00%] [Memory Beats: 70.73%] - LeetPush

--- 189-Rotate-Array.py
+++ 189-Rotate-Array.py
@@ -3,12 +3,10 @@ public:
     void rotate(vector<int>& nums, int k) {
         int n = nums.size();
         k = k%n;
-        if(k==0) return;
-        vector<int> temp(n);
-        for(int i =0;i<n;i++){
-            temp[(i+k)%n] = nums[i];
-        }
-        nums=temp;
+        int m = n-k;
+        reverse(nums.begin(),nums.end());
+        reverse(nums.begin(),nums.begin()+k);
+        reverse(nums.begin()+k,nums.end());
         
     }
 };
